f5cac5c54a2b958a0993460c44edbb51cf403d0e
aryan102006 <76812277+aryan102006@users.noreply.github.com>
2021-01-19 17:40:31 +0530

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,16 +1,19 @@
-public class FizzBuzz {
-	public String FizzBuzz(integer i) {
+import java.util.Scanner;
 
+public class FizzBuzz {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
 		String result = "";
 		if (i % 3 == 0) {
-    	result += "Fizz";
+				result += "Fizz";
 		}
 		if (i % 5 == 0) {
-    	result += "Buzz";
+			result += "Buzz";
 		}
 		if (result.equals("")) {
-    	result = i.toString();
+			result = Integer.toString(i);
 		}
-		return result;
+		System.out.println(result);
 	}
-}
+}	
